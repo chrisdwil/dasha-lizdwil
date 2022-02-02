99d20b288a85df2b8b7d878c6a4228abf3a0b623
chrisdwil <cwilkerson@Chriss-MacBook-Pro.local>
2022-02-01 23:40:50 -0600

    adding new files, also changed license to MIT to match original author

---
+++ app/main.dsl
@@ -0,0 +1,43 @@
+context {
+    input phone: string;
+    input forward: string? = null;
+}
+
+start node root
+{
+    do
+    {
+        #connectSafe($phone);
+        #waitForSpeech(1000);
+        #say("hello");
+        wait *;
+    }
+    transitions
+    {
+        @exit: goto @exit on true;
+    }
+}
+
+node @exit
+{
+    do
+    {
+        if ($forward is not null)
+        {
+            #say("forward");
+            #forward($forward);
+            exit;
+        }
+        #say("fine");
+        exit;
+    }
+}
+
+digression @exit_dig
+{
+    conditions { on true tags: onclosed; }
+    do
+    {
+        exit;
+    }
+}
---
+++ index.js
@@ -0,0 +1,66 @@
+const commander = require("commander");
+const dasha = require("@dasha.ai/sdk");
+
+commander
+  .command("out")
+  .description("check calls from Dasha")
+  .requiredOption("-p --phone <phone>", "phone or SIP URI to call to")
+  .option("-c --config <name>", "SIP config name", "default")
+  .option("-f --forward <phone>", "phone or SIP URI to forward the call to")
+  .option("-v --verbose", "Show debug logs")
+  .action(async ({ phone, config, forward, verbose }) => {
+    const app = await dasha.deploy("./app");
+
+    await app.start();
+
+    const conv = app.createConversation({ phone, forward: forward ?? null });
+    if (verbose) {
+      conv.on("debugLog", console.log);
+    }
+    conv.audio.tts = "dasha";
+    conv.sip.config = config;
+    await conv.execute();
+
+    await app.stop();
+    app.dispose();
+  });
+
+commander
+  .command("in")
+  .description("check calls to Dasha")
+  .option("-f --forward <phone>", "phone or SIP URI to forward the call to")
+  .option("-v --verbose", "Show debug logs")
+  .action(async ({ forward, verbose }) => {
+    const app = await dasha.deploy("./app", { groupName: "Default" });
+    app.queue.on("ready", async (id, conv, info) => {
+      if (info.sip !== undefined)
+        console.log(`Captured sip call: ${JSON.stringify(info.sip)}`);
+      conv.input = { phone: "", forward: forward ?? null };
+      if (verbose === true) {
+        conv.on("debugLog", console.log);
+      }
+      conv.audio.tts = "dasha";
+      await conv.execute();
+    });
+
+    await app.start();
+
+    console.log("Waiting for calls via SIP");
+    const config = (await dasha.sip.inboundConfigs.listConfigs())[
+      "sip-test-app"
+    ];
+    if (config?.applicationName === "sip-test-app") {
+      console.log(config?.uri);
+    }
+    console.log("Press Ctrl+C to exit");
+    console.log(
+      "More details: https://docs.dasha.ai/en-us/default/tutorials/sip-inbound-calls/"
+    );
+    console.log("Or just type:");
+    console.log(
+      "dasha sip create-inbound --application-name sip-test-app sip-test-app"
+    );
+    console.log("And call to sip uri returned by command above");
+  });
+
+commander.parseAsync();
